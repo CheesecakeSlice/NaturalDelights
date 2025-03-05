42dd0a558b0c28c3c1ca6691a04941b63af90468
CheesecakeSlice <safmanjoel@gmail.com>
2025-03-06 00:41:31 +0100

    Initial Commit

---
+++ src/client/java/me/CheesecakeSlice/naturaldelights/client/NaturaldelightsClient.java
@@ -0,0 +1,10 @@
+package me.CheesecakeSlice.naturaldelights.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NaturaldelightsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/me/CheesecakeSlice/naturaldelights/Naturaldelights.java
@@ -0,0 +1,10 @@
+package me.CheesecakeSlice.naturaldelights;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Naturaldelights implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
