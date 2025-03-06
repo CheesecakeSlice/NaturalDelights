722e3f604e01f9793c956f435f166392f4ec971f
CheesecakeSlice <safmanjoel@gmail.com>
2025-03-06 01:13:09 +0100

    Logger added to ModInitializer - publicly accessible

--- src/main/java/me/CheesecakeSlice/naturaldelights/Naturaldelights.java
+++ src/main/java/me/CheesecakeSlice/naturaldelights/Naturaldelights.java
@@ -5,7 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Naturaldelights implements ModInitializer {
-
+    // Logger allows for consol output using LOGGER.info/warn/error/debug to display a message in the console
     public static final Logger LOGGER = LoggerFactory.getLogger("natural-delights");
 
     @Override
